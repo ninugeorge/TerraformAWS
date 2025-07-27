f8d6d81231a46297dde3326e47e35d628b191e2e
ninugeorge <ninu.george@gmail.com>
2025-07-27 10:45:48 -0400

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,3 +9,12 @@ resource "aws_iam_user" "kumardemo2" {
   name          = "kumardemo100${count.index}"
   force_destroy = "true"
 }
+terraform {
+  backend "s3" {
+    bucket = "mybucket4terraform-ninu"
+    key    = "terraform.tfstate"
+    region =  "us-east-1"
+    profile = "default"
+    shared_credentials_files = ["~/credentials"]
+  }
+}
